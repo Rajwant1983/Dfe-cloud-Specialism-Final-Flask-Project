2de1351d00cf8422c854981872067f5a120d17c4
Rajwant <rajwantkaurthethi1983@gmail.com>
2022-08-30 19:03:48 +0100

    commit

---
+++ Part 1 Dfe Flask App/c9WebFlask/tests/test_app.py
@@ -0,0 +1,21 @@
+from typing_extensions import Self
+from unicodedata import name
+from flask_testing import LiveServerTestCase
+from urllib.request import urlopen
+from flask import url_for
+
+# this is testing 3
+from app import app,musicDBConnection
+
+class TestBase(LiveServerTestCase):
+    TEST_PORT=5050 #test port,does not need to be open
+
+
+def musicDBConnection():  
+    app.config.update(
+        SQLALCHEMY_DATABASE_URI="sqlite:///",
+        LIVESERVER_PORT=Self.TEST_PORT,            
+        DEBUG=True,
+        TESTING=True
+    )
+    return app
