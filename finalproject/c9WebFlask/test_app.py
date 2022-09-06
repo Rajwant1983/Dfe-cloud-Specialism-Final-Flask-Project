64d169068734f931d9eb5bc36f8e81a9a4ffae22
Rajwant <rajwantkaurthethi1983@gmail.com>
2022-09-06 15:28:01 +0100

    changes

--- finalproject/c9WebFlask/test_app.py
+++ finalproject/c9WebFlask/test_app.py
@@ -1,25 +1,25 @@
-from unicodedata import name
-from flask_testing import LiveServerTestCase
-from urllib.request import urlopen
-from flask import url_for
+#from unicodedata import name
+#from flask_testing import LiveServerTestCase
+#from urllib.request import urlopen
+#from flask import url_for
 
 # this is testing 3
-from app import app,musicDBConnection
+#from app import app,musicDBConnection
 
-class TestBase(LiveServerTestCase):
-    TEST_PORT=5050 #test port,does not need to be open
+#class TestBase(LiveServerTestCase):
+ #   TEST_PORT=5050 #test port,does not need to be open
 
 
-def musicDBConnection():  
-    app.config.update(
-        SQLALCHEMY_DATABASE_URI="sqlite:///",
-        LIVESERVER_PORT=Self.TEST_PORT,            
-        DEBUG=True,
-        TESTING=True
-    )
-    return app
-def setUp(self):
-    from app import c7Music
-    musicDBConnection.app() 
+#def musicDBConnection():  
+   # app.config.update(
+    #    SQLALCHEMY_DATABASE_URI="sqlite:///",
+     #   LIVESERVER_PORT=Self.TEST_PORT,            
+      #  DEBUG=True,
+       # TESTING=True
+   # )
+    #return app
+#def setUp(self):
+ #   from app import c7Music
+  #  musicDBConnection.app() 
 
    
